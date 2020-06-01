9ac31026f62660e97e4a3ee218ba7f3747e44996
bm@krishna <bodimalla.mohan@lendfoundry.com>
2020-06-01 19:49:25 +0530

    add comments where you need to improve and resove issue

--- corejava/interface/bank.java
+++ corejava/interface/bank.java
@@ -1,36 +1,45 @@
-import java.io.*;
-import java.util.*;
-interface Bank
+
+// nice good example and good work 
+
+// i mention some points check it in commnets
+
+// import java.io.*;  what is the use of this imports 
+// import java.util.*; what is the use of this imports
+interface BankI // allwasy give end I like BankI
 {
-    float p=1000;
-    float t=2;
-    public void rateofinterest();
+    float p = 1000; // - variables by default public final abstract
+    float t = 2;
+
+    public void rateofinterest(); // method name should be allways camelCase
 }
-class SBI implements Bank
-{
-    float r=9/100;
-    public void rateofinterest()
-    {
-        float i=(p*t*r)/100;
-        System.out.println("the rate of interest"+i);
+
+class SBI implements BankI {
+    float r = 9 / 100f;
+
+    public void rateofinterest() {
+        float i = (p * t * r) / 100;
+        System.out.println(i);
+        System.out.println(t);
+        System.out.println(p);
+        System.out.println("the rate of interest  " + i);
     }
 }
-class HDFC implements Bank
-{
-    float r1=10/100;
-    public void rateofinterest()
-    {
-        float i1=(p*t*r1)/100;
-        System.out.println("the rate of interest"+i1);
+
+class HDFC implements BankI {
+    float r1 = 10 / 100f;
+
+    public void rateofinterest() {
+
+        float i1 = (p * t * r1) / 100;
+        System.out.println("the rate of interest   " + i1);
     }
 }
-public class bank{
-    public static void main(String[] args){
-        SBI sbi=new SBI();
+
+public class bank { // class Name should be allways caps firt letter Bank
+    public static void main(String[] args) {
+        SBI sbi = new SBI();
         sbi.rateofinterest();
-        HDFC hdfc=new HDFC();
+        HDFC hdfc = new HDFC();
         hdfc.rateofinterest();
     }
 }
-
-
